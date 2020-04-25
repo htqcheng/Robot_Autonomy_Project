9aede8bde3c9a935f254ab2066444c652d0e8cfe
Shaun-CMU <sryer@andrew.cmu.edu>
2020-04-25 13:42:12 -0400

    merged some of bryson's code

--- helper.py
+++ helper.py
@@ -1,5 +1,78 @@
+'''
+This file contains helper functions for the 16-662 Robot Autonomy
+Final Project. This was completed by Bryson Jones, Quentin Cheng,
+and Shaun Ryer.
+Carnegie Mellon University
+MRSD Class of 2021
+'''
+
 import numpy as np
+import scipy as sp
 from quaternion import from_rotation_matrix, quaternion, as_euler_angles, from_euler_angles, as_float_array
+import cv2
+import imutils
+from imutils import perspective
+from imutils import contours
+
+def generate_bounding_box(rgb_img, lower_bound, upper_bound):
+    '''
+    :param rgb_img: an rgb image with an object that should have a bounding
+                    box overlaid onto it
+    :param lower_bound: lower thresholding bound (1 x 3)
+    :param upper_bound: upper thresholding bound (1 x 3)
+    :return: bgr_modified_img: image in bgr format, which has bounding box
+                               overlaid
+    '''
+
+    # convert rgb to hsv
+    bgr = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2BGR)
+
+    # create bounded mask
+    mask_red = cv2.inRange(bgr, lower_bound, upper_bound)
+
+    # bitwise mask
+    redBoxOnlyFrame = cv2.bitwise_and(bgr.astype(np.uint8), bgr.astype(np.uint8), mask=mask_red)
+
+    # convert image to gray
+    gray = cv2.cvtColor(redBoxOnlyFrame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    # perform edge detection, then perform a dilation + erosion to
+    # close gaps in between object edges
+    edges = cv2.Canny(gray, 50, 100)
+    edges = cv2.dilate(edges, None, iterations=1)
+    edges = cv2.erode(edges, None, iterations=1)
+
+    # find contours in the edge map
+    cnts = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    # sort the contours from left-to-right and initialize the
+    # 'pixels per metric' calibration variable
+    (cnts, _) = contours.sort_contours(cnts)
+
+    # loop over the contours individually
+    bgr_modified_img = bgr.copy()
+    for c in cnts:
+        # if the contour is not sufficiently large, ignore it
+        if cv2.contourArea(c) < 100:
+            continue
+        # compute the rotated bounding box of the contour
+        box = cv2.minAreaRect(c)
+        box = cv2.boxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+        box = np.array(box, dtype="int")
+        # order the points in the contour such that they appear
+        # in top-left, top-right, bottom-right, and bottom-left
+        # order, then draw the outline of the rotated bounding
+        # box
+        box = perspective.order_points(box)
+        cv2.drawContours(bgr_modified_img, [box.astype("int")], -1, (0, 255, 0), 2)
+
+    cv2.imwrite('test.jpg', bgr_modified_img)
+
+    return bgr_modified_img
+
+
+
 
 
 
@@ -86,4 +159,68 @@ def pick_up_box(state,obs,gripper,small_container0_obj,z,small_container_pos,sma
         action = np.concatenate((obs.gripper_pose, np.array([1])))
 
     return state,action
+
+
+def get_objects(state, shape, obs, object_pos, box_pos):
+    stepsize = 0.01
+    if state == 0:
+        if abs(object_pos[0] - obs.gripper_pose[0]) > .008 or abs(object_pos[1] - obs.gripper_pose[1]) > .008:
+            movementVector = np.asarray([(object_pos[0] - obs.gripper_pose[0]),
+                                         (object_pos[1] - obs.gripper_pose[1]),
+                                         .1 * (object_pos[2] - obs.gripper_pose[2])])
+        else:
+            movementVector = np.asarray([(object_pos[0] - obs.gripper_pose[0]),
+                                         (object_pos[1] - obs.gripper_pose[1]),
+                                         (object_pos[2] - obs.gripper_pose[2])])
+
+
+        if np.linalg.norm(movementVector) < 0.006:
+            state = 1
+            return [0, 0, 0, 0, 0, 0, 1, 0], state, shape
+
+        unitMovementVector = movementVector / np.linalg.norm(movementVector)
+        robotStep = unitMovementVector * stepsize
+        delta_quat = np.asarray([0, 0, 0, 1])  # xyzw
+        gripper_pos = np.asarray([1])
+
+        return np.concatenate((robotStep, delta_quat, gripper_pos)), state, shape
+
+    elif state == 1:
+        if obs.gripper_pose[2] > .95:
+            state = 2
+        return [0, 0, stepsize, 0, 0, 0, 1, 0], state, shape
+
+    elif state == 2:
+        # This was code to try and determine whether the object was actually picked up or not. It causes an error right now
+        # if np.linalg.norm(np.asarray([(object_pos[0] - obs.gripper_pose[0]),
+        #                               (object_pos[1] - obs.gripper_pose[1]),
+        #                               (object_pos[2] - obs.gripper_pose[2])])) > .1:
+        #     state = 0
+
+        if abs(box_pos[0] - obs.gripper_pose[0]) > .005 or abs(box_pos[1] - obs.gripper_pose[1]) > .005:
+            movementVector = np.asarray([(box_pos[0] - obs.gripper_pose[0]),
+                                         (box_pos[1] - obs.gripper_pose[1]),
+                                         0])
+        else:
+            movementVector = np.asarray([(box_pos[0] - obs.gripper_pose[0]),
+                                         (box_pos[1] - obs.gripper_pose[1]),
+                                         (box_pos[2] - obs.gripper_pose[2])])
+
+
+        unitMovementVector = movementVector / np.linalg.norm(movementVector)
+        robotStep = unitMovementVector * stepsize
+        delta_quat = np.asarray([0, 0, 0, 1])  # xyzw
+        gripper_pos = np.asarray([0])
+
+        if np.linalg.norm(movementVector) < 0.05:
+            state = 3
+            return [0, 0, 0, 0, 0, 0, 1, 1], state, shape
+
+        return np.concatenate((robotStep, delta_quat, gripper_pos)), state, shape
+
+    elif state == 3:
+        if obs.gripper_pose[2] > 1:
+            state = 0
+            shape = str(int(shape) + 2)
+        return [0, 0, stepsize, 0, 0, 0, 1, 1], state, shape
         
--- rlbench_empty_container.py
+++ rlbench_empty_container.py
@@ -7,13 +7,13 @@ from rlbench.action_modes import ArmActionMode, ActionMode
 from rlbench.observation_config import ObservationConfig
 from rlbench.tasks import *
 
-from helper import pick_up_box, pick_up_box_variables
+from helper import pick_up_box, pick_up_box_variables, generate_bounding_box, get_objects
+import helper 
 
 def skew(x):
     return np.array([[0, -x[2], x[1]],
                     [x[2], 0, -x[0]],
                     [-x[1], x[0], 0]])
-
 
 def sample_normal_pose(pos_scale, rot_scale):
     '''
@@ -27,7 +27,19 @@ def sample_normal_pose(pos_scale, rot_scale):
 
     return pos, quat_wxyz
 
+class MoveAgent:
 
+    def act(self, obs, target_pos):
+        stepsize = 0.015
+        movementVector = np.asarray([(target_pos[0]-obs.gripper_pose[0]),
+                                     (target_pos[1]-obs.gripper_pose[1]),
+                                     (target_pos[2]-obs.gripper_pose[2])])
+        unitMovementVector = movementVector / np.linalg.norm(movementVector)
+        robotStep = unitMovementVector * stepsize
+        delta_quat = np.asarray([0, 0, 0, 1]) # xyzw
+        gripper_pos = np.asarray([0])
+
+        return np.concatenate((robotStep, delta_quat, gripper_pos))
 
 class NoisyObjectPoseSensor:
 
@@ -63,13 +75,18 @@ class NoisyObjectPoseSensor:
 
 
 if __name__ == "__main__":
-    action_mode = ActionMode(ArmActionMode.ABS_EE_POSE_PLAN) # See rlbench/action_modes.py for other action modes
+
+    mode = 0
+    if mode ==1:
+        action_mode = ActionMode(ArmActionMode.ABS_EE_POSE_PLAN) # See rlbench/action_modes.py for other action modes
+    else: action_mode = ActionMode(ArmActionMode.DELTA_EE_POSE) 
     env = Environment(action_mode, '', ObservationConfig(), False)
     task = env.get_task(EmptyContainer) # available tasks: EmptyContainer, PlayJenga, PutGroceriesInCupboard, SetTheTable
-   
+    agent = MoveAgent()
     obj_pose_sensor = NoisyObjectPoseSensor(env)
-    state = 0
+    update = 0
     count = 0
+    
 
     gripper = task._robot.gripper
     objs = obj_pose_sensor.get_objs()
@@ -78,10 +95,18 @@ if __name__ == "__main__":
             small_container0_obj = obj
 
     descriptions, obs = task.reset()
-    # print(descriptions)
+    print(descriptions)
+    global state
+    state = 0
+    global shape
+    shape = '0'
+
+    # initialize shape poses
+    obj_poses = obj_pose_sensor.get_poses()
+    shape_pos = obj_poses['Shape' + shape][:3]
+    most_recent_shape_pos = shape_pos
 
     # Getting noisy object poses
-    obj_poses = obj_pose_sensor.get_poses()
     small_container_pos = obj_poses['small_container0'][:3]
     small_container_pos_original = obj_poses['small_container0'][:3]
     large_container_pos = obj_poses['large_container'][:3]
@@ -89,34 +114,52 @@ if __name__ == "__main__":
     small_container_pos[2] -= 0.01
     small_container_quat = quaternion(obj_poses['small_container0'][3], obj_poses['small_container0'][4], obj_poses['small_container0'][5],obj_poses['small_container0'][6])
     small_container_euler = as_euler_angles(small_container_quat)
-    # print(small_container_euler)
+
     z = small_container_euler[0]
 
     above_large_container,small_container_pos_original,notflipped_array,flipped_array = pick_up_box_variables(large_container_pos,obs,z,small_container_pos)
 
     while True:
-        # Get shapes
-        shape0_pos = obj_poses['Shape'][:3]
-        shape1_pos = obj_poses['Shape1'][:3]
-        shape3_pos = obj_poses['Shape3'][:3]
+        if mode==0:
 
-        # The size of the small container is .25x by .14y by .07z (m)
-        small_container_pos[0] += 0.070*np.sin(z)
-        small_container_pos[1] += 0.070*np.cos(z)
+            # Getting noisy object poses
+            obj_poses = obj_pose_sensor.get_poses()
+            small_container_pos = obj_poses['small_container0'][:3]
+
+
+            # Perform action and step simulation
+            if int(shape) < 5:  # shape goes from 0, 2, 4
+                if state == 0:
+                    try:
+                        shape_pos = obj_poses['Shape' + shape][:3]
+                        most_recent_shape_pos = shape_pos
+                    except KeyError:
+                        shape_pos = most_recent_shape_pos
+                elif state == 2:
+                    try:
+                        shape_pos = obj_poses['Shape' + shape][:3]
+                        most_recent_shape_pos = shape_pos
+                    except KeyError:
+                        shape_pos = most_recent_shape_pos
+                else:
+                    shape_pos = [0, 0, 0]
+
+
+            action, state, shape = get_objects(state, shape, obs, shape_pos, small_container_pos)
         
-        # Getting various fields from obs
-        current_joints = obs.joint_positions
-        gripper_pose = obs.gripper_pose
-        rgb = obs.wrist_rgb
-        depth = obs.wrist_depth
-        mask = obs.wrist_mask
-        
-        state,action = pick_up_box(state,obs,gripper,small_container0_obj,z,small_container_pos,small_container_pos_original,gripper_pose,above_large_container,flipped_array,notflipped_array)
-    
+        if mode == 1:
+            call_once = 0
+            if call_once == 0:
+                pass
+            obj_poses = obj_pose_sensor.get_poses()
+            
+            current_joints = obs.joint_positions
+            gripper_pose = obs.gripper_pose
+            small_container_pos[0] += 0.070*np.sin(z)
+            small_container_pos[1] += 0.070*np.cos(z)
+            update,action = pick_up_box(update,obs,gripper,small_container0_obj,z,small_container_pos,small_container_pos_original,gripper_pose,above_large_container,flipped_array,notflipped_array)
+
             
         obs, reward, terminate = task.step(action)
-
-        # if terminate:
-        #     break
 
     env.shutdown()
